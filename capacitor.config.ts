22677b90fd203ee9f33d6c5971166f6744816054
ALISAM <sofia09alyssa@gmail.com>
2026-09-14 15:49:39 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Organization-List',
+  webDir: 'dist'
+};
+
+export default config;
